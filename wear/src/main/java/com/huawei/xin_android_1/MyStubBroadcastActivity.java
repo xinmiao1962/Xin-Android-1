4b6bcbf659693a857485f4eb1c8bdb0cddcf5a1a
x80035882 <xin.miao@huawei.com>
2015-07-16 17:40:51 -0500

    Initial commit

---
+++ wear/src/main/java/com/huawei/xin_android_1/MyStubBroadcastActivity.java
@@ -0,0 +1,22 @@
+package com.huawei.xin_android_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.huawei.xin_android_1.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
